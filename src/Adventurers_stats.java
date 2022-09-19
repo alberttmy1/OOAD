fddf603ec04bd4c5803a005bd8aa0cd7de67995a
alberttmy1 <77512061+alberttmy1@users.noreply.github.com>
2022-09-18 21:39:07 -0600

    day1

---
+++ src/Adventurers_stats.java
@@ -0,0 +1,51 @@
+public class Adventurers_stats {
+
+    private int hp;
+    private String name;
+    private int id;
+    private int atk;
+    private String loc;
+    private int treasure;
+
+    public Adventurers_stats(int num){
+        hp  = 3;
+        atk = 1;
+        treasure = 0;
+        loc = "0-1-1";
+        if(num == 1){
+            name = "Brawlers";
+            id = 1;
+        } else if (num == 2) {
+            name = "Sneakers ";
+            id = 2;
+        }else if (num == 3){
+            name = "Runners";
+            id = 3;
+        }else{
+            name = "Thieves";
+            id = 4;
+        }
+    }
+    public Integer getID(){
+        return id;
+    }
+
+    //return the spawn coordinates
+    public String getSpawn(){
+        return loc;
+    }
+
+    //
+    public void setHP(int health){
+        hp = health;
+    }
+
+    public void setSpawn(String place){
+        loc = place;
+    }
+
+    public Integer getTreasure(){return treasure;}
+
+    public void setTreasure(int money){ treasure = money;}
+
+}
