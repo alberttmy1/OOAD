c23facedf3ea06485582e6afeba89626eb429888
KevinFLoaiza <kelo2418@colorado.edu>
2022-09-27 18:57:23 -0600

    New files added

---
+++ project3/src/Logger.java
@@ -0,0 +1,12 @@
+//import java.util.Observable;
+//import java.util.Observer;
+//
+//public class Logger implements Observer {
+//    private String inputs;
+//
+//    public void update(Observable obj, Object arg) {
+//        inputs += ((String) arg);
+//        System.out.println("It ran");
+//    }
+//}
+
---
+++ project3/src/battle3.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ ** out = 0 -> then no one took dmg
+ ** out = 1 -> then creature took dmg
+ ** out = 2 -> hero took dmg
+ */
+
+public class battle3 extends Celebrate{
+
+    static int [] fight(int[] Hlocation, int Hid, int Hhp, String name, int Chp, int[] Clocation, List<String> Hinv){
+        searchAndCombat fight = new searchAndCombat();
+        Celebrate celeb = new Celebrate();
+        int battle_res;
+        if(Hid == 1 || Hid == 2 || Hid == 4){
+            battle_res = fight.combat(Hid, Hinv);
+            if(Arrays.equals(Hlocation, Clocation) && !(Arrays.equals(Clocation, new int[]{0, 0, 0}))) {
+                if (battle_res == 0) {
+                    return new int[]{Hhp, Chp};
+                }
+                if (battle_res == 1) {
+                    Chp--;
+                    System.out.print(name+ " celebrates: " + celeb.Cheers());
+                    return new int[]{Hhp, Chp};
+                }
+                if (battle_res == 2) {
+                    Hhp--;
+                    return new int[]{Hhp, Chp};
+                }
+            }
+        }
+        else{
+            if(Arrays.equals(Hlocation, Clocation) && !(Arrays.equals(Clocation, new int[]{0, 0, 0}))) {
+                int[] health = new int[]{0, 0};
+                for(int i = 0; i < 2; i++) {
+                    battle_res = fight.combat(Hid, Hinv);
+                    if (battle_res == 0) {
+                        health = new int[]{Hhp, Chp};
+                    }
+                    if (battle_res == 1 && Chp != 0) {
+                        Chp--;
+                        System.out.print(name+ " celebrates: " + celeb.Cheers());
+                        health = new int[]{Hhp, Chp};
+                    }
+                    if (battle_res == 2) {
+                        Hhp--;
+                        health = new int[]{Hhp, Chp};
+                    }
+                }
+                return health;
+            }
+        }
+        return new int[]{Hhp, Chp};
+    }
+}
